9243f4d4873d3bc519b94ada5fc55e1ee0e29321
barakaganai <barakaganai01@gmail.com>
2022-08-06 15:17:32 +0300

    import topbar component on app js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import About from "./About";
 import "./App.css";
 import Home from "./home/Home";
+import TopBar from "./topbar/TopBar";
 
 function App() {
   return (
     <div className="App">
+      <TopBar />
       <Home />
+      <About />
     </div>
   );
 }
