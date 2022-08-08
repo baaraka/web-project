2d35451aff5f103299ae8b415a5b26ec5b697ddf
barakaganai <barakaganai01@gmail.com>
2022-08-09 00:39:26 +0300

    import gallery component on app jsx component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import About from "./About";
 import "./App.css";
+import Gallery from "./gallery/Gallery";
 import Home from "./home/Home";
 import Services from "./services/Services";
 import Team from "./team/Team";
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Services />
       <Team />
+      <Gallery />
     </div>
   );
 }
