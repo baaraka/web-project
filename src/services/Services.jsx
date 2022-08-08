9affaa949e0d9f8d230632181711c46bf0715067
barakaganai <barakaganai01@gmail.com>
2022-08-08 17:48:39 +0300

    create and write services jsx component

---
+++ src/services/Services.jsx
@@ -0,0 +1,105 @@
+import "./Services.css";
+
+export default function Services() {
+  return (
+    <div className="services">
+      <div className="serviceContainer">
+        <div className="serviceContainerHeading">
+          <h1 className="heading">Our Services</h1>
+        </div>
+        <div className="serviceContainers">
+          <div className="containerService">
+            <div className="serviceContainerList">
+              <i className="icon fa-solid fa-bath"></i>
+            </div>
+            <div className="containerHeading">
+              <h2 className="headings">Residential Cleaning</h2>
+              <p className="paragraph">
+                I am a full-stack web developer with strong knowledge of
+                MongoDB, node.js, Javascript and front end technologies such
+                react, jquery, Ejs and counting.Graduated University with degree
+                in science of information technology. Strong mathematics and
+                programming background.
+              </p>
+            </div>
+          </div>
+          <div className="containerService">
+            <div className="serviceContainerList">
+              <i className="icon fa-solid fa-pump-medical"></i>
+            </div>
+            <div className="containerHeading">
+              <h2 className="headings">Residential Cleaning</h2>
+              <p className="paragraph">
+                I am a full-stack web developer with strong knowledge of
+                MongoDB, node.js, Javascript and front end technologies such
+                react, jquery, Ejs and counting.Graduated University with degree
+                in science of information technology. Strong mathematics and
+                programming background.
+              </p>
+            </div>
+          </div>
+          <div className="containerService">
+            <div className="serviceContainerList">
+              <i className="icon fa-solid fa-soap"></i>
+            </div>
+            <div className="containerHeading">
+              <h2 className="headings">Residential Cleaning</h2>
+              <p className="paragraph">
+                I am a full-stack web developer with strong knowledge of
+                MongoDB, node.js, Javascript and front end technologies such
+                react, jquery, Ejs and counting.Graduated University with degree
+                in science of information technology. Strong mathematics and
+                programming background.
+              </p>
+            </div>
+          </div>
+          <div className="containerService">
+            <div className="serviceContainerList">
+              <i className="icon fa-solid fa-broom"></i>
+            </div>
+            <div className="containerHeading">
+              <h2 className="headings">Residential Cleaning</h2>
+              <p className="paragraph">
+                I am a full-stack web developer with strong knowledge of
+                MongoDB, node.js, Javascript and front end technologies such
+                react, jquery, Ejs and counting.Graduated University with degree
+                in science of information technology. Strong mathematics and
+                programming background.
+              </p>
+            </div>
+          </div>
+          <div className="containerService">
+            <div className="serviceContainerList">
+              <i className="icon fa-solid fa-snowplow"></i>
+            </div>
+            <div className="containerHeading">
+              <h2 className="headings">Residential Cleaning</h2>
+              <p className="paragraph">
+                I am a full-stack web developer with strong knowledge of
+                MongoDB, node.js, Javascript and front end technologies such
+                react, jquery, Ejs and counting.Graduated University with degree
+                in science of information technology. Strong mathematics and
+                programming background.
+              </p>
+            </div>
+          </div>
+          <div className="containerService">
+            <div className="serviceContainerList">
+              <i className="icon fa-solid fa-pump-soap"></i>
+            </div>
+            <div className="containerHeading">
+              <h2 className="headings">Residential Cleaning</h2>
+              <p className="paragraph">
+                I am a full-stack web developer with strong knowledge of
+                MongoDB, node.js, Javascript and front end technologies such
+                react, jquery, Ejs and counting.Graduated University with degree
+                in science of information technology. Strong mathematics and
+                programming background.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
