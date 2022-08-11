e0ccd7bfe0cba460d4375b9d64ff539566b1360b
barakaganai <barakaganai01@gmail.com>
2022-08-11 23:49:35 +0300

    create and write says jsx component

---
+++ src/says/Says.jsx
@@ -0,0 +1,72 @@
+import "./Says.css";
+
+export default function Says() {
+  return (
+    <div className="says">
+      <div className="saysItem">
+        <div className="saysHeading">
+          <h1 className="saysHead">What People Say About Us</h1>
+        </div>
+        <div className="saysBox">
+          <div className="sayBox1">
+            <div className="saysBoxImg">
+              <img
+                src="https://t4.ftcdn.net/jpg/02/65/60/81/360_F_265608151_dW8v74JWjL0ch4iJ5l2oyPm4kv2WBrqe.jpg"
+                alt=""
+                className="saysBoxItemImg sayImg1"
+              />
+            </div>
+            <div className="sayBoxItem1">
+              <p className="saysParagraph">
+                I am a full-stack web developer with strong knowledge of
+                MongoDB, node.js, Javascript and front end technologies such
+                react, jquery, Ejs and counting.Graduated University with degree
+                in science of information technology. Strong mathematics and
+                programming background.
+              </p>
+              <span className="saysSpan">Barack Ganai</span>
+            </div>
+          </div>
+          <div className="sayBox2">
+            <div className="saysBoxImg">
+              <img
+                src="https://t4.ftcdn.net/jpg/02/65/60/81/360_F_265608151_dW8v74JWjL0ch4iJ5l2oyPm4kv2WBrqe.jpg"
+                alt=""
+                className="saysBoxItemImg sayImg2"
+              />
+            </div>
+            <div className="sayBoxItem2">
+              <p className="saysParagraph">
+                I am a full-stack web developer with strong knowledge of
+                MongoDB, node.js, Javascript and front end technologies such
+                react, jquery, Ejs and counting.Graduated University with degree
+                in science of information technology. Strong mathematics and
+                programming background.
+              </p>
+              <span className="saysSpan">Barack Ganai</span>
+            </div>
+          </div>
+          <div className="sayBox3">
+            <div className="saysBoxImg">
+              <img
+                src="https://t4.ftcdn.net/jpg/02/65/60/81/360_F_265608151_dW8v74JWjL0ch4iJ5l2oyPm4kv2WBrqe.jpg"
+                alt=""
+                className="saysBoxItemImg"
+              />
+            </div>
+            <div className="sayBoxItem2">
+              <p className="saysParagraph">
+                I am a full-stack web developer with strong knowledge of
+                MongoDB, node.js, Javascript and front end technologies such
+                react, jquery, Ejs and counting.Graduated University with degree
+                in science of information technology. Strong mathematics and
+                programming background.
+              </p>
+              <span className="saysSpan">Barack Ganai</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
