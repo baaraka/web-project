36caa375a1cdcfd23606065129ff004974e008a7
barakaganai <barakaganai01@gmail.com>
2022-08-16 23:45:28 +0300

    create questions jsx component

---
+++ src/questions/Questions.jsx
@@ -0,0 +1,48 @@
+import "./Questions.css";
+
+export default function Questions() {
+  return (
+    <div className="questions">
+      <div className="questionsDiv">
+        <div className="questionsHeading">
+          <h1 className="questionsDivHeading">Frequently Asked Questions</h1>
+        </div>
+        <div className="questionsList">
+          <div className="questionsListItem">
+            <p className="questionsParagraph">
+              I am a full-stack web developer with strong knowledge of MongoDB,
+              node.js, Javascript and front end technologies such react, jquery,
+              Ejs and counting.
+            </p>
+            <p className="questionsParagraphItem">
+              I am a full-stack web developer with strong knowledge of MongoDB,
+              node.js, Javascript and front end technologies such react, jquery,
+              Ejs and counting.
+            </p>
+          </div>
+          <div className="questionsListItem">
+            <p className="questionsParagraph">
+              I am a full-stack web developer with strong knowledge of MongoDB,
+              node.js, Javascript and front end technologies such react, jquery,
+              Ejs and counting.
+            </p>
+          </div>
+          <div className="questionsListItem">
+            <p className="questionsParagraph">
+              I am a full-stack web developer with strong knowledge of MongoDB,
+              node.js, Javascript and front end technologies such react, jquery,
+              Ejs and counting.
+            </p>
+          </div>
+          <div className="questionsListItem">
+            <p className="questionsParagraph">
+              I am a full-stack web developer with strong knowledge of MongoDB,
+              node.js, Javascript and front end technologies such react, jquery,
+              Ejs and counting.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
