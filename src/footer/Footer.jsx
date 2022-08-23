6fa02443d4bab01fc0dd4d11dc132248c3889501
barakaganai <barakaganai01@gmail.com>
2022-08-23 22:06:18 +0300

    update Footer.jsx component

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -6,12 +6,13 @@ export default function Footer() {
       <div className="footerDiv">
         <div className="footerDivList">
           <div className="footerDivListItem">
-            <div className="footerListItemImg">
+            <div className="footerListItemLeft">
               <img
                 src="https://img.freepik.com/premium-vector/letter-og-os-luxury-simple-logo-design-with-gold-color-best-company-premium-product_563735-24.jpg"
                 alt=""
                 className="footerImg"
-              />
+              />{" "}
+              <br />
               <span className="footerSpan">
                 I am a full-stack web developer with strong knowledge of
                 MongoDB, node.js, Javascript and front end technologies such
@@ -28,6 +29,21 @@ export default function Footer() {
             </div>
             <div className="footerRight">
               <span className="footerSpanRight">Contact</span>
+              <br />
+              <i className="footIcons fa-solid fa-envelope"></i>
+              <span className="footerEmail">contact@9barack.com</span>
+              <br />
+              <i className="footIcon fa-solid fa-square-phone"></i>
+              <i className="footIcon fa-brands fa-facebook"></i>
+              <i className="footIcon fa-brands fa-twitter"></i>
+              <i className="footIcon fa-brands fa-instagram"></i>
+            </div>
+          </div>
+          <div className="footerBottom">
+            <div className="footerBottomItem">
+              <span className="footerBottomItemList">
+                Copyright ⓒ 2022 Caregreek
+              </span>
             </div>
           </div>
         </div>
