f0793a4cd3f31408dd102b267e72441b750bd1a1
barakaganai <barakaganai01@gmail.com>
2022-08-26 13:08:42 +0300

    add some div on home jsx component

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -16,8 +16,28 @@ export default function Home() {
             <p className="homeParagraph">SHEDULE SERVICES</p>
           </div>
         </div>
-
+        <div className="homeContainerCenter">
+          <div className="homeContainerImgItem">
+            <img
+              src="https://thumbs.dreamstime.com/b/portrait-handsome-young-african-american-black-man-combing-his-hair-bathroom-isolated-over-grey-background-185903660.jpg"
+              alt=""
+              className="homeContainerImgCenter"
+            />
+          </div>
+          <div className="homeContainerCenterParagraph">
+            <span className="centerParagraph">Barack Ganai</span>
+            <p className="homeCenterParagraph">
+              I am a full-stack web developer
+            </p>
+          </div>
+        </div>
         <div className="homeContainerImgSection">
+          <div className="homeContainerDash">
+            <ul className="homeDash">
+              <li>............</li>
+              <li>............</li>
+            </ul>
+          </div>
           <div className="homeContainerImg">
             <img
               src="https://media.istockphoto.com/photos/young-trendy-man-in-glasses-and-sweater-feeling-happy-about-in-text-picture-id1237258345?k=20&m=1237258345&s=612x612&w=0&h=9t9BCsdPN18XkfkBiq6tDDnxi-CR7Kb3NfYRMSx2yMw="
