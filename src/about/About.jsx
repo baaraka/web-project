bb3b7d3f55c0b59840efcbc27b57086ba36c74a9
barakaganai <barakaganai01@gmail.com>
2022-08-26 20:29:08 +0300

    update about.jsx component

---
+++ src/about/About.jsx
@@ -0,0 +1,85 @@
+import "./About.css";
+
+export default function About() {
+  return (
+    <div className="about">
+      <div className="aboutUs">
+        <div className="aboutUsItemSm">
+          <div className="aboutImgItem div1">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeeFvemQoSczl617Omfgg2Snoh0tfYLoObeOMerhA5Ejcn4f53TY68cKSMU6W1k76dQXA&usqp=CAU"
+              alt=""
+              className="img1"
+            />
+          </div>
+          <div className="aboutImgItem div2">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWVWUiMRdBgvV-T9CXr4Dyw4jWFhU-wfnDjaok-o_xq_NA62NpLeaPUehLfJaA8Jrvu0s&usqp=CAU"
+              alt=""
+              className="img2"
+            />
+          </div>
+          <div className="aboutImgItem div3">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMC8T2ybj8WCpXsgY4f1hBpi4SnXuoGR0JUTHavFN94T_5KiZhOVluheuWF5jeOZICiDk&usqp=CAU"
+              alt=""
+              className="img3"
+            />
+          </div>
+        </div>
+        <div className="aboutUsItems">
+          <h1 className="aboutUsItem">About Us</h1>
+          <p className="aboutUsItemParagraph">
+            Professional iOS Developer,Fluent in Swift and Objective C
+            Programming languages. I am also a fullstack web developer with
+            strong knowledge of MongoDB, node.js, Javascript and front end
+            technologies such react, jquery, Ejs and and counting. Graduated
+            University with degree in Electrical & Electronics Engineering.
+            Strong mathematics and programming background.
+          </p>
+        </div>
+      </div>
+      <div className="aboutUsWhy">
+        <div className="aboutUsHeading">
+          <h1 className="aboutHeadingWhy">Why caregreek</h1>
+          <div className="aboutIconDiv">
+            <div className="aboutUsIconDiv">
+              <i className="aboutUsIcon fa-solid fa-check"></i>
+            </div>
+            <span className="aboutUHeadingParagraph">
+              Professional iOS Developer,Fluent in Swift and Objective C
+              Programming languages. I am also a fullstack web developer
+            </span>
+          </div>
+          <div className="aboutIconDiv">
+            <div className="aboutUsIconDiv">
+              <i className="aboutUsIcon fa-solid fa-check"></i>
+            </div>
+            <span className="aboutUHeadingParagraph">
+              Professional iOS Developer,Fluent in Swift and Objective C
+              Programming languages. I am also a fullstack web developer
+            </span>
+          </div>
+          <div className="aboutIconDiv">
+            <div className="aboutUsIconDiv">
+              <i className="aboutUsIcon fa-solid fa-check"></i>
+            </div>
+            <span className="aboutUHeadingParagraph">
+              Professional iOS Developer,Fluent in Swift and Objective C
+              Programming languages. I am also a fullstack web developer
+            </span>
+          </div>
+        </div>
+        <div className="aboutUsWhyVideo">
+          <div className="aboutUsVideo">
+            <iframe
+              width="450"
+              height="250"
+              src="https://www.youtube.com/embed/tgbNymZ7vqY?playlist=tgbNymZ7vqY&loop=1"
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
