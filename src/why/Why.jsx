02fa130eecb44c432fb6b7789a0c90d86bb5bb23
barakaganai <barakaganai01@gmail.com>
2022-08-31 10:42:30 +0300

    create Why.jsx component

---
+++ src/why/Why.jsx
@@ -0,0 +1,49 @@
+import "./Why.css";
+
+export default function Why() {
+  return (
+    <div className="why">
+      <div className="aboutUsWhy">
+        <div className="aboutUsHeading">
+          <h1 className="aboutHeadingWhy">Why caregreek</h1>
+          <div className="aboutIconDiv">
+            <div className="aboutUsIconDiv">
+              <i className="aboutUsIcon fa-solid fa-check"></i>
+            </div>
+            <span className="aboutUHeadingParagraph">
+              Professional iOS Developer,Fluent in Swift and Objective C
+              Programming languages. I am also a fullstack web developer
+            </span>
+          </div>
+          <div className="aboutIconDiv">
+            <div className="aboutUsIconDiv">
+              <i className="aboutUsIcon fa-solid fa-check"></i>
+            </div>
+            <span className="aboutUHeadingParagraph">
+              Professional iOS Developer,Fluent in Swift and Objective C
+              Programming languages. I am also a fullstack web developer
+            </span>
+          </div>
+          <div className="aboutIconDiv">
+            <div className="aboutUsIconDiv">
+              <i className="aboutUsIcon fa-solid fa-check"></i>
+            </div>
+            <span className="aboutUHeadingParagraph">
+              Professional iOS Developer,Fluent in Swift and Objective C
+              Programming languages. I am also a fullstack web developer
+            </span>
+          </div>
+        </div>
+        <div className="aboutUsWhyVideo">
+          <div className="aboutUsVideo">
+            <iframe
+              width="450"
+              height="250"
+              src="https://www.youtube.com/embed/tgbNymZ7vqY?playlist=tgbNymZ7vqY&loop=1"
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
