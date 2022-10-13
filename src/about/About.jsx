74ad624468b608e36b9b7ac9b0a6e3d9b477bab8
barakaganai <barakaganai01@gmail.com>
2022-10-13 20:35:14 +0300

    update paragraph of about.jsx

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,4 +1,3 @@
-import ReadMoreLessAbout from "../readMoreLessAbout/ReadMoreLessAbout";
 import "./About.css";
 
 export default function About() {
@@ -30,24 +29,13 @@ export default function About() {
         </div>
         <div className="aboutUsItems">
           <h1 className="aboutUsItem">About Us</h1>
-          <ReadMoreLessAbout limit={200}>
-            <p className="aboutUsItemParagraph">
-              Caregreek International Limited Was Established In 2018 And Has
-              Over The Years Evolved To Become The Largest And Most Specialized
-              Cleaning Company In Abuja. It is One Of Wholly Citizen Owned
-              Commercial Cleaning Company In Nigeria. The Company Registration
-              Number Is RC1509998. It Has Extensive Experience In Cleaning All
-              Types Of Building Such AS Schools, Office, Industrial
-              Building(Premises), Hotels, Shopping Center, Government Offices,
-              Commercial Premises And Hospitals, Bank And Others. <hr /> The
-              Company's Success Lies In Its Ability To Train And Manage Hundred
-              Of Stuffs To Archive The Stringent Standard Demanded By Customers
-              We Serve In Nigeria. <hr /> Our Services Include Provision Of
-              Janitorial Services, Hygiene Supplies, Pest Control, Once
-              Off-Specialized Cleaning Office Plant Supplies And Landscaping And
-              Garden Services And Chemical Supplies.
-            </p>
-          </ReadMoreLessAbout>
+          <p className="aboutUsItemParagraph">
+            Caregreek International Limited Was Established In 2018 And Has Over
+            The Years Evolved To Become The Largest And Most Specialized
+            Cleaning Company In Abuja. It is One Of Wholly Citizen Owned
+            Commercial Cleaning Company In Nigeria.
+          </p>
+          <button className="aboutBtn">Read More</button>
         </div>
       </div>
     </div>
