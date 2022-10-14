ffb921f8dcca53f5d0dec4df51e848ec29b60344
barakaganai <barakaganai01@gmail.com>
2022-10-14 20:55:43 +0300

    update heading and paragraph

--- src/team/Team.jsx
+++ src/team/Team.jsx
@@ -5,13 +5,12 @@ export default function Team() {
     <div className="team">
       <div className="teamDiv">
         <div className="teamHeading">
-          <h1 className="heading">Our Team</h1>
+          <h1 className="headingTeam">Our Team</h1>
           <p className="teamParagraph">
-            I am a full-stack web developer with strong knowledge of MongoDB,
-            node.js, Javascript and front end technologies such react, jquery,
-            Ejs and counting.Graduated University with degree in science of
-            information technology. Strong mathematics and programming
-            background.
+            Our Employees Are Critical To Services We Provide And We Are Very
+            Selective In Taking On New Staff. Our Staffs Has An Excellent Track
+            Record And Many Years Of Experience And They Are Provided With
+            Regular Training.
           </p>
         </div>
         <div className="teamImg">
