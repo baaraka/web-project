98031842574e7250102ef0f74aa17d7633e5f7b7
barakaganai <barakaganai01@gmail.com>
2022-10-14 20:54:39 +0300

    change heading and paragraph

--- src/services/Services.jsx
+++ src/services/Services.jsx
@@ -15,11 +15,10 @@ export default function Services() {
             <div className="containerHeading">
               <h2 className="headings">Residential Cleaning</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus,
+                ipsam ab incidunt culpa odit corrupti nobis et officia molestias
+                quo aut exercitationem non, vero quisquam maxime atque vel
+                repudiandae dolorem.
               </p>
             </div>
           </div>
@@ -28,13 +27,12 @@ export default function Services() {
               <i className="icon fa-solid fa-pump-medical"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Commercial Cleaning</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Obcaecati cumque dicta iusto inventore amet possimus. Est minima
+                necessitatibus molestiae vero esse rerum rem voluptates ipsam
+                soluta, reiciendis nobis veritatis aspernatur.
               </p>
             </div>
           </div>
@@ -43,13 +41,12 @@ export default function Services() {
               <i className="icon fa-solid fa-soap"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Post Construction Cleaning</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Voluptatem porro provident sapiente, perferendis quod totam
+                facere. Maiores autem nobis voluptas temporibus, saepe omnis
+                sapiente. Atque rerum quas accusantium quam accusamus.
               </p>
             </div>
           </div>
@@ -58,13 +55,12 @@ export default function Services() {
               <i className="icon fa-solid fa-broom"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Fumigation/ Pest Control</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+                voluptatibus eaque, itaque dicta dignissimos excepturi
+                asperiores cumque impedit? Velit maiores fugiat vitae tenetur
+                dolore illo nobis officia, saepe reprehenderit eum.
               </p>
             </div>
           </div>
@@ -73,13 +69,12 @@ export default function Services() {
               <i className="icon fa-solid fa-snowplow"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Carpet and Upholstering Cleaning</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Dolores incidunt voluptates delectus, provident, doloremque nemo
+                culpa tempore sed, nam eius cupiditate aut at fugit? Iste error
+                natus laboriosam laudantium tempora.
               </p>
             </div>
           </div>
@@ -88,18 +83,17 @@ export default function Services() {
               <i className="icon fa-solid fa-pump-soap"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">End Of Tenancy Cleaning</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam
+                praesentium quis consequatur molestias repellat? Ipsam
+                consequatur id numquam suscipit at animi maxime ratione
+                cupiditate iure nostrum sit, natus similique repellendus.
               </p>
             </div>
           </div>
         </div>
-        <button className="serviceBtn">View More</button>
+        <button className="serviceBtn">VIEW MORE</button>
       </div>
     </div>
   );
